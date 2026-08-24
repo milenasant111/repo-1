ed018e25caddb7d0eac95cbd02f6c73eff1481d1
milenasant111 <milenasant111@gmail.com>
2026-08-24 17:13:01 -0300

    main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+package AAA;
+import javax.swing.*;
+public class main {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <21; i++) {
+			JOptionPane.showMessageDialog(null, i);
+		}
+			
+			
+
+	}
+
+}
